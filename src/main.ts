337382a176a251492670c07c6c1ebe61abd76b4b
Jutipong <techcom.ec20@gmail.com>
2024-07-10 17:47:06 +0700

    refactor: Remove unused files and update imports

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,17 @@
-// import { createApp } from 'vue'
-// import './style.css'
-// import App from './App.vue'
-// import vuetify from './plugins/vuetify';
-// import router from '@/@core/router'
-
-// const app = createApp(App);
-// app.use(router as any);
-// app.use(vuetify).mount('#app');
-
 import { createApp } from 'vue'
-// import '@/assets/css/styles/index.scss'
-// import '@/scss/style.scss';
 
 import type { PluginOptions } from 'vue-toastification'
 import Toast from 'vue-toastification'
 import PerfectScrollbar from 'vue3-perfect-scrollbar'
+
 import App from './App.vue'
-// import vuetify from '@/@core/plugins/vuetify'
+import vuetify from '@/@core/plugins/vuetify'
 import router from '@/@core/router'
 
-// import '@/assets/css/main.css'
+import '@/scss/style.scss'
 
 import 'vue-toastification/dist/index.css'
 import 'virtual:uno.css'
-
-import '@/scss/style.scss'
 
 document.title = import.meta.env.VITE_APP_TITLE
 
