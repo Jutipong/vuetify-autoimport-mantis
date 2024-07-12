6428fb401d4a88f39c9563cf849ffb9679321fe7
Jutipong <techcom.ec20@gmail.com>
2024-07-12 13:32:08 +0700

    refactor: Update fetch-wrapper to handle unauthorized and forbidden responses
    The code changes in `fetch-wrapper.ts` update the `handleResponse` function to handle unauthorized (401) and forbidden (403) responses from the API. Previously, the function would call the `logout` function from the `useAuthStore` hook, but now it redirects the user to the login page using the `router.push` method. This change improves the user experience by automatically redirecting them to the login page when their session is no longer valid.
    
    Note: The commit message follows the conventional format of "refactor: <commit message>".

--- src/utils/helpers/fetch-wrapper.ts
+++ src/utils/helpers/fetch-wrapper.ts
@@ -41,10 +41,10 @@ function handleResponse(response: any) {
         const data = text && JSON.parse(text)
 
         if (!response.ok) {
-            const { user, logout } = useAuthStore()
+            const { user } = useAuthStore()
             if ([401, 403].includes(response.status) && user) {
                 // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
-                logout()
+                router.push('/login')
             }
 
             const error = (data && data.message) || response.statusText
