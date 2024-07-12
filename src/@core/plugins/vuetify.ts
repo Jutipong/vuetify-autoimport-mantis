2135f0bf103f33aa660c81b0bfff4fc44ccb01f1
Jutipong <techcom.ec20@gmail.com>
2024-07-12 16:10:42 +0700

    refactor: Update Vuetify imports and styles
    This commit updates the Vuetify imports in the `vuetify.ts` file to include additional components from the `labsComponents` module. It also modifies the styles in the `_VCard.scss` file to add an outlined variant for the `VCard` component and adjust the background color. Additionally, it updates the container styles in the `_container.scss` file to remove padding on smaller screens.
    
    Note: The commit message follows the conventional format of "refactor: <commit message>".

--- src/@core/plugins/vuetify.ts
+++ src/@core/plugins/vuetify.ts
@@ -2,12 +2,12 @@ import { createVuetify } from 'vuetify'
 import '@mdi/font/css/materialdesignicons.css'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import * as labsComponents from 'vuetify/labs/components'
 import { DefaultTheme } from '@/@core/plugins/DefaultTheme'
 
 export default createVuetify({
-    components,
+    components: { ...components, ...labsComponents },
     directives,
-
     theme: {
         defaultTheme: 'DefaultTheme',
         themes: {
@@ -17,6 +17,7 @@ export default createVuetify({
     defaults: {
         VBtn: {},
         VCard: {
+            variant: 'outlined',
             rounded: 'md',
         },
         VTextField: {
