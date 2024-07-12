1413595f4f874ec6b8d341d0c1e78d5293ce92a5
Jutipong <techcom.ec20@gmail.com>
2024-07-12 14:47:13 +0700

    refactor: Update imports for color-related components

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,15 @@ import { createApp } from 'vue'
 
 import type { PluginOptions } from 'vue-toastification'
 import Toast from 'vue-toastification'
-import PerfectScrollbar from 'vue3-perfect-scrollbar'
+import { PerfectScrollbarPlugin } from 'vue3-perfect-scrollbar'
 
 import App from './App.vue'
+
 import vuetify from '@/@core/plugins/vuetify'
 import router from '@/@core/router'
 
 import './assets/scss/style.scss'
-
+import 'vue3-perfect-scrollbar/style.css'
 import 'vue-toastification/dist/index.css'
 import 'virtual:uno.css'
 
@@ -18,7 +19,7 @@ document.title = import.meta.env.VITE_APP_TITLE
 const app = createApp(App)
 app.use(vuetify)
 app.use(router)
-app.use(PerfectScrollbar)
+app.use(PerfectScrollbarPlugin)
 app.use(piniaConfig)
 app.use(Toast, {
     transition: 'Vue-Toastification__bounce',
